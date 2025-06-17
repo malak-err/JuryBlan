175b3ad6a951b6825e462a773775c67a575fd4d8
 <erraoudymalak1997@gmail.com>
2025-06-17 09:50:04 +0100

    conseption uml

---
+++ src/test/java/com/example/deliverymatch/DeliveryMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deliverymatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryMatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
